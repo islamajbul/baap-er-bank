5ef361b92191d62c4a908de8b3e062b232e7bf56
islamajabul <ajabulmd1@gmail.com>
2023-02-17 01:29:51 +0800

    done

--- js/withdraw.js
+++ js/withdraw.js
@@ -6,26 +6,38 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithdrawAmountString = withdrawField.value;
     // use paseFloat
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-    
+
     // step 03
+    withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+        alert('Please input a valid number');
+        return
+    }
+    
+    // step 04
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
     console.log(previousWithdrawTotal);
     
-    // step 04
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-    
     // step 05
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('Insuficient Balance');
+        return;
+    }
     
     // step 06
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 07
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step 07
-    withdrawField.value = '';
+    
 })
